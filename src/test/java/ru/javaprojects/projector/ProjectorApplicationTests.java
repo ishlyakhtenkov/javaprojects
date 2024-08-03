fb76aaf0185657e4effc7c03282a8f9aeef71471
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2024-08-03 20:37:17 +0300

    init commit

---
+++ src/test/java/ru/javaprojects/projector/ProjectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.projector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

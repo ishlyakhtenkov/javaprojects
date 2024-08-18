0c709cac8f09dc8bfda4c15daed5b478a270efd4
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2024-08-18 10:31:59 +0300

    Customize bootstrap selector

---
+++ src/main/resources/static/js/bootstrap-selector-float.js
@@ -0,0 +1,49 @@
+const selector = $('#selector');
+const selectorLabel = $('#selectorLabel');
+let selectorFormControl = null;
+let selectorTitle = null;
+
+selector.on('loaded.bs.select', () => {
+    selectorFormControl = $('.bootstrap-select.form-control');
+    selectorTitle = $('.filter-option-inner-inner');
+
+    $('.dropdown-toggle.btn-white').addClass('border-0 pt-0').attr('style', 'outline: none !important')
+    if (selector.val().length !== 0) {
+        selectorTitle.removeClass('text-muted');
+        selectorLabel.attr('hidden', false);
+        selectorFormControl.removeClass('pt-2').addClass('pt-3');
+    } else {
+        selectorTitle.addClass('text-muted');
+    }
+})
+
+selector.on('shown.bs.select', () => {
+    selectorFormControl.addClass('border-primary-subtle').attr('style', 'box-shadow: 0 0 0 0.25rem rgba(0, 110, 255, 0.25)');
+    selectorLabel.attr('hidden', false);
+    if (selector.val().length === 0) {
+        selectorTitle.html('&nbsp;');
+    } else {
+        selectorTitle.removeClass('text-muted');
+    }
+})
+
+
+selector.on('changed.bs.select', () => {
+    if (selector.val().length === 0) {
+        selectorFormControl.removeClass('pt-3').addClass('pt-2');
+        selectorTitle.addClass('text-muted');
+        selectorTitle.html('&nbsp;');
+    } else {
+        selectorTitle.removeClass('text-muted');
+        selectorFormControl.removeClass('pt-2').addClass('pt-3');
+        selectorLabel.attr('hidden', false);
+    }
+})
+
+selector.on('hide.bs.select',() => {
+    selectorFormControl.removeClass('border-primary-subtle').attr('style', '');
+    if (selector.val().length === 0) {
+        selectorLabel.attr('hidden', true);
+        selectorTitle.addClass('text-muted').html(selector.prop('title'));
+    }
+})
